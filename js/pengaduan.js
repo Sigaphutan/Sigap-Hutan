cf4dc099da103a9c59f8e7edf5ae25abce22b709
Sigaphutan <rebbecazeb777@gmail.com>
2026-07-19 13:04:32 +0700

    Update pengaduan.js

--- js/pengaduan.js
+++ js/pengaduan.js
@@ -1,1 +1,16 @@
+import { db } from "./firebase.js";
 
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const form = document.getElementById("formPengaduan");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    alert("Berhasil terhubung dengan Firebase!");
+
+});
