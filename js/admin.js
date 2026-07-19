6a7ae8632866d3dd640ccb270ba5f916db0dfd14
Sigaphutan <rebbecazeb777@gmail.com>
2026-07-19 12:23:03 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,33 @@
+import { auth } from "./firebase.js";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
+const provider = new GoogleAuthProvider();
+
+// Cek apakah admin sudah login
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("Login berhasil");
+        console.log(user.displayName);
+    } else {
+        console.log("Belum login");
+    }
+});
+
+// Fungsi Login
+window.loginAdmin = async function () {
+    try {
+        await signInWithPopup(auth, provider);
+    } catch (error) {
+        alert(error.message);
+    }
+};
+
+// Fungsi Logout
+window.logoutAdmin = async function () {
+    await signOut(auth);
+};
