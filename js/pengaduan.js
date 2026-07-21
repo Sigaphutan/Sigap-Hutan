22221e12dbec7e5095164f7f4ced9e8ed4823f19
Sigaphutan <rebbecazeb777@gmail.com>
2026-07-21 12:46:04 +0700

    Update pengaduan.js

--- js/pengaduan.js
+++ js/pengaduan.js
@@ -1,10 +1,15 @@
-import { db } from "./firebase.js";
+import { db, storage } from "./firebase.js";
 
 import {
     collection,
     addDoc,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-storage.js";
 
 console.log("pengaduan.js berhasil dimuat");
 
